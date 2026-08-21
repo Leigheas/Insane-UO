70dbc1704d92846366847c3f42dda7ef70d7879e
Leigheas <63628116+Leigheas@users.noreply.github.com>
2026-08-21 12:29:13 -0500

    updated spam prevention
    made it not even try to chop another tree till you have moved from where you were standing

--- Scripts/walking_lumberjack.py
+++ Scripts/walking_lumberjack.py
@@ -258,11 +258,11 @@ while True:
         
     if Player.Hits >= Player.HitsMax:
         if Journal.SearchByType( "There's not enough wood here to harvest.", 'System' ):
-            #x, y = Player.Position.X, Player.Position.Y 
+            x, y = Player.Position.X, Player.Position.Y 
             Player.HeadMessage(2125, "Tree is empty!!")
-            #while Player.Position.X == x and Player.Position.Y == y:
-            #    Misc.Pause(3000)
-            Misc.Pause(5000)
+            while Player.Position.X == x and Player.Position.Y == y:
+                Misc.Pause(100)
+            Misc.Pause(1000)
             Journal.Clear()
         else:
             Target.TargetResource(AXE_SERIAL, "wood")
